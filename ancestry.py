cc2efe1b39cc6d45caee8d3082a962f30845e746
prodromospapa <prodromospapa@gmail.com>
2023-10-30 20:16:54 +0200

    main

--- ancestry.py
+++ ancestry.py
@@ -22,22 +22,18 @@ def create_ancestry(term,ancestry_url,api_key,definition,onto_terms,output,task,
         else:
             childs.append(i)
     childs = childs[::-1]
-        
     for checking_format in [parent.title(),parent.lower(),parent.upper()]:
         onto_parent = onto.search(label = checking_format)
         if onto_parent:
             parent = checking_format
-
-
-
     for child in childs[:-1]:
-        child_def = ancestor_details[child][0]
+        child_def = ancestor_details[child.replace(" ","_")][0]
         if child_def==[]:
             child_def = get_annotation(api_key,child,task,onto_ontologies)
             if child_def == 0:
                 child_def = askstring("Set definition", f'''Can't find definition for "{child}"''')               
         else:
-            child_def = child_def[0]+ f"({ancestor_details[child][1]})"
+            child_def = child_def[0]+ f"({ancestor_details[child.replace(' ','_')][1]})"
         child = child.replace("_"," ")
         create_class(child,parent,child_def,onto,ontology_name)#vazei tous parental orous
         parent = child
